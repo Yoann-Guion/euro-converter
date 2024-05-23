34376fb5ae7e7d5b8bc1569fcb24eec326c39931
Yoann-Guion <yoo.guion@gmail.com>
2024-05-23 13:30:41 +0200

    le state change bien, maintenant faut changer la classe d'App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Header from './Header/Header';
 import Currencies from './Currencies/Currencies';
 import Result from './Result/Result';
@@ -7,9 +9,20 @@ import currencies from '../../data/currencies';
 import './App.scss';
 
 function App() {
+  // Mode dark :
+  const [isDark, setIsDark] = useState(false);
+
   return (
     <div className="App">
       <Header />
+      <button
+        type="button"
+        onClick={() => {
+          setIsDark(!isDark);
+        }}
+      >
+        Dark mode
+      </button>
       <Currencies currencies={currencies} />
       <Result />
     </div>
